bf47186050dfe221d0abce41d4da737abb84f0bd
Wisdom Erhimwionsbo <20097898@mail.wit.ie>
2025-01-24 14:43:32 +0000

    Typed function signature.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,2 +1,46 @@
 import {Friend, Colleague } from './myTypes'
 import { friends, colleagues } from "./01-basics";
+
+function older(f: Friend) : string {
+    f.age += 1
+    return `${f.name} is now ${f.age}` 
+}
+
+console.log(older(friends[0]))
+
+function allOlder(friends: Friend[]): string[] {
+    return friends.map(friend => {
+        friend.age += 1;
+        return `${friend.name} is now ${friend.age}`;
+    });
+}
+
+console.log(allOlder(friends));
+
+function highestExtension(cs: Colleague[]): Colleague {
+    const result = cs.sort(
+      (c1, c2) => c1.contact.extension - c2.contact.extension
+    );
+    return result[cs.length - 1];
+  }
+  console.log(highestExtension(colleagues.current));
+
+  function addColleague(
+    colleaguesArray: Colleague[],
+    name: string,
+    department: string,
+    email: string
+): void {
+    const highest = highestExtension(colleaguesArray).contact.extension;
+    const newColleague: Colleague = {
+        name,
+        department,
+        contact: {
+            email,
+            extension: highest + 1,
+        },
+    };
+    colleaguesArray.push(newColleague);
+}
+addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
